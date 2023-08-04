6c4e678e023cc540be0133c7a2353381af83cc47
Arya Prakash <aryaprak@uw.edu>
2023-08-03 20:06:23 -0800

    condensing
    attempting to condense to pyppeteer

--- main.py
+++ main.py
@@ -1,38 +1,83 @@
+import asyncio
+from pyppeteer import launch
 import openai
 
+# Set your OpenAI API key here
+openai.api_key = open("cgpt4 key.txt", "r").read()
 
-def read_api_key(file_path):
-    with open(file_path,'r') as file:
-        text_context = file.read()
-    return text_content.strip()
-file_path = 'cgpt4 key.txt'
-text_content = read_api_key(file_path)
-openai.api_key = text_content
+async def scrape_with_puppeteer(url):
+    browser = await launch(headless=True)
+    page = await browser.newPage()
 
-def write_to_js(file_path, text_content):
-    with open(file_path, 'w') as file:
-        file.write(text_content)
-js_file_path = 'output.js'
-write_to_js(js_file_path, text_content)
+    try:
+        # Set a timeout of 30 seconds for the page to load
+        await page.goto(url, {'waitUntil': 'domcontentloaded', 'timeout': 30000})
+        content = await page.evaluate('document.body.innerText')
+    except Exception as e:
+        print("Error occurred during scraping:", str(e))
+        content = ""
+    finally:
+        await browser.close()
 
+    return content
 
-read_api_key()
-write_to_js()
+def generate_summary(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=200
+    )
+    return response['choices'][0]['text']
 
-chat_log= []
+def search_website(topic, website_name=None):
+    openai_search_query = f"{topic} website"
+    if website_name:
+        openai_search_query += f" on {website_name}"
+    search_results = openai.Engine("ada").search(
+        model="text-davinci-002",
+        query=openai_search_query,
+        documents=3,
+        return_prompt=True
+    )
+    for result in search_results["data"]:
+        if result["score"] > 5.0:
+            return result["document"]["text"]
 
-while True:
-    user_message = input()
-    if user_message.lower() == "quit":
-        print(chat_log)
-        break
-    else:
-        chat_log.append({"role" : "user", "content": user_message})
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=chat_log
-        )
+    return None
 
-        assistant_response = response['choices'][0]['message']['content']
-        print("ChatGPT:", assistant_response.strip("\n").strip())
-        chat_log.append({"role": "assistant", "content": assistant_response.strip("\n").strip()})
+if __name__ == "__main__":
+    user_topic = input("Enter the topic you want analytics/trends on: ")
+    user_website = input("Enter the website URL or name you want to scrape (press Enter if none): ").strip()
+
+    try:
+        # Search for the website using OpenAI
+        website_url = search_website(user_topic, user_website)
+
+        if not website_url:
+            print(f"No relevant website found for '{user_topic}'.")
+            scraped_content = ""
+        else:
+            # Scrape the website using pyppeteer
+            loop = asyncio.get_event_loop()
+            scraped_content = loop.run_until_complete(scrape_with_puppeteer(website_url))
+
+            # Rest of the code for filtering and summarizing results
+            sentences = scraped_content.split(". ")
+            # Filter relevant sentences based on the user's topic
+            relevant_sentences = [sentence for sentence in sentences if user_topic.lower() in sentence.lower()]
+
+            if not relevant_sentences:
+                print("No relevant information found on the given topic.")
+            else:
+                print("Relevant information:")
+                for sentence in relevant_sentences:
+                    print(sentence)
+
+                # Summarize each relevant result using OpenAI
+                summarized_results = [generate_summary(sentence) for sentence in relevant_sentences]
+
+                print("\nSummarized results:")
+                for summary in summarized_results:
+                    print(summary)
+    except Exception as e:
+        print("Error occurred:", str(e))
